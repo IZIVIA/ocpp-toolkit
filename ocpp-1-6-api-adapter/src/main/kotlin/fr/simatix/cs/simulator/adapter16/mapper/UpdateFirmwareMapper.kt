e93207501bf3fa2a5c586da4f0362867f67b6956
Jeremie Grimal <djeremie.grimal@free.fr>
2022-04-11 14:07:44 +0200

    feat-updatefirmware - fix:  add some annotations for the updatefirmware message

--- ocpp-1-6-api-adapter/src/main/kotlin/fr/simatix/cs/simulator/adapter16/mapper/UpdateFirmwareMapper.kt
+++ ocpp-1-6-api-adapter/src/main/kotlin/fr/simatix/cs/simulator/adapter16/mapper/UpdateFirmwareMapper.kt
@@ -3,9 +3,12 @@ package fr.simatix.cs.simulator.adapter16.mapper
 import fr.simatix.cs.simulator.api.model.updatefirmware.FirmwareType
 import fr.simatix.cs.simulator.core16.model.updatefirmware.UpdateFirmwareReq
 import fr.simatix.cs.simulator.core16.model.updatefirmware.UpdateFirmwareResp
+import org.mapstruct.Mapper
+import org.mapstruct.ReportingPolicy
 import fr.simatix.cs.simulator.api.model.updatefirmware.UpdateFirmwareReq as UpdateFirmwareReqGen
 import fr.simatix.cs.simulator.api.model.updatefirmware.UpdateFirmwareResp as UpdateFirmwareRespGen
 
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 abstract class UpdateFirmwareMapper {
 
     fun coreToGenReq(updateFirmwareReq: UpdateFirmwareReq): UpdateFirmwareReqGen{
