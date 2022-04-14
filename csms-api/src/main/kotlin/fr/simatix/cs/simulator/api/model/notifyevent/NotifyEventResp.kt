6f8ff0c7af7b04b7fc602fa13253a54817917477
Deborah Pereira <deborah.pereira@4sh.fr>
2022-04-14 16:44:52 +0200

    feat: add NotifyEvent model in csms-api

---
+++ csms-api/src/main/kotlin/fr/simatix/cs/simulator/api/model/notifyevent/NotifyEventResp.kt
@@ -0,0 +1,5 @@
+package fr.simatix.cs.simulator.api.model.notifyevent
+
+import fr.simatix.cs.simulator.api.model.Response
+
+class NotifyEventResp : Response
---
+++ csms-api/src/main/kotlin/fr/simatix/cs/simulator/api/model/notifyevent/enumeration/EventTriggerEnumType.kt
@@ -0,0 +1,9 @@
+package fr.simatix.cs.simulator.api.model.notifyevent.enumeration
+
+enum class EventTriggerEnumType(val value: String) {
+    Alerting("Alerting"),
+
+    Delta("Delta"),
+
+    Periodic("Periodic");
+}
