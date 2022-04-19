a02edf902052ceeef19751763803102d6171b79e
Deborah Pereira <deborah.pereira@4sh.fr>
2022-04-19 13:57:33 +0200

    feat: EVSIM-62 make CertificateSignedReq CertificateSignedResp extends Request/Response

--- csms-api/src/main/kotlin/fr/simatix/cs/simulator/api/model/certificateSigned/CertificateSignedResp.kt
+++ csms-api/src/main/kotlin/fr/simatix/cs/simulator/api/model/certificateSigned/CertificateSignedResp.kt
@@ -1,9 +1,10 @@
 package fr.simatix.cs.simulator.api.model.certificateSigned
 
+import fr.simatix.cs.simulator.api.model.Response
 import fr.simatix.cs.simulator.api.model.certificateSigned.enumeration.CertificateSignedStatusEnumType
 import fr.simatix.cs.simulator.api.model.common.StatusInfoType
 
 data class CertificateSignedResp(
     val status: CertificateSignedStatusEnumType,
     val statusInfo: StatusInfoType? = null
-)
+): Response
