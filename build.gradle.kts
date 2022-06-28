fd167541b8b5a66531b548fab5b94cd7fc179eda
Elian ORIOU <elian.oriou@4sh.fr>
2022-06-28 20:24:39 +0200

    added missing sonatype config

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,3 +90,14 @@ subprojects {
         onlyIf { System.getenv("GPG_PRIVATE_KEY") != null }
     }
 }
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(System.getenv("SONATYPE_USERNAME"))
+            password.set(System.getenv("SONATYPE_PASSWORD"))
+        }
+    }
+}
