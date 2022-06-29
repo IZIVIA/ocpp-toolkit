e2b4f71c1dd997dcaec35628f1d5db6e719bd12e
ymalle <yaya-externe.malle@izivia.com>
2022-06-29 11:25:46 +0200

    feat: IDEV-54 adding ocpp 1-5 api project

---
+++ ocpp-1-5-api/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    kotlin("jvm")
+    kotlin("kapt")
+    java
+    `maven-publish`
+}
+
+coreProject()
+
+dependencies {
+    implementation(project(":ocpp-1-5-core"))
+    implementation(project(":operation-information"))
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        named<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "ocpp-1-5-api"
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                name.set("OCPP 1.5 API")
+                artifactId = "ocpp-1-5-api"
+                description.set("OCPP 1.5 API")
+            }
+        }
+    }
+}
