ff2562a85137ba35457acd593c315501805542bf
ymalle <yaya-externe.malle@izivia.com>
2022-06-29 11:26:21 +0200

    feat: IDEV-54 adding ocpp 1-5 core project

---
+++ ocpp-1-5-core/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("jvm")
+    java
+    `maven-publish`
+}
+
+coreProject()
+
+dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:_")
+    implementation(project(":utils"))
+    implementation(project(":ocpp-transport"))
+    implementation(project(":operation-information"))
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        named<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "ocpp-1-5-core"
+            version = project.version.toString()
+
+            from(components["java"])
+
+            pom {
+                name.set("OCPP 1.5 Core")
+                artifactId = "ocpp-1-5-core"
+                description.set("OCPP 1.5 Core")
+            }
+        }
+    }
+}
