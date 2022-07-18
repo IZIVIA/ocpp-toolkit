24cbb4da29e8e7757b4cccaedcc7870538867414
vincent.pichon <vincent.pichon@izivia.com>
2022-07-18 12:12:30 +0200

    IDEV-54 Fix fun name

--- ocpp-soap/src/main/kotlin/com/izivia/ocpp/soap/OcppSoapMapper.kt
+++ ocpp-soap/src/main/kotlin/com/izivia/ocpp/soap/OcppSoapMapper.kt
@@ -3,7 +3,6 @@ package com.izivia.ocpp.soap
 import com.fasterxml.jackson.annotation.JsonInclude.Include
 import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule
 import com.fasterxml.jackson.dataformat.xml.XmlFactory
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
@@ -13,14 +12,14 @@ import javax.xml.stream.XMLInputFactory
 
 
 object OcppSoapMapper : ObjectMapper(
-    XmlMapper(get_new_factory(true),  CustomXmlModule)
+    XmlMapper(getNewFactory(true),  CustomXmlModule)
         .registerModule(kotlinModule())
         .registerModule(KotlinxInstantModule())
         .setSerializationInclusion(Include.NON_NULL)
         .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 )
 
-fun get_new_factory(ns_aware: Boolean) : XmlFactory {
+fun getNewFactory(ns_aware: Boolean) : XmlFactory {
     val CustomXmlFactory: XMLInputFactory = XMLInputFactory.newFactory()
     CustomXmlFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, ns_aware)
     return XmlFactory(CustomXmlFactory)
