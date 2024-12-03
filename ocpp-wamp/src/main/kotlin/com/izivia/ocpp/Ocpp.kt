4764ab6b3cc1c4584ba08038da00fd89faf638a3
Edward Poot <edwardmp@gmail.com>
2024-12-03 17:20:05 +0100

    Change 2.0 subprotocol to be 2.0.1, as 2.0 is only a draft

--- ocpp-wamp/src/main/kotlin/com/izivia/ocpp/Ocpp.kt
+++ ocpp-wamp/src/main/kotlin/com/izivia/ocpp/Ocpp.kt
@@ -3,5 +3,5 @@ package com.izivia.ocpp
 typealias CSOcppId = String
 
 enum class OcppVersion(val subprotocol:String) {
-    OCPP_1_5("ocpp1.5"), OCPP_1_6("ocpp1.6"), OCPP_2_0("ocpp2.0")
+    OCPP_1_5("ocpp1.5"), OCPP_1_6("ocpp1.6"), OCPP_2_0("ocpp2.0.1")
 }
