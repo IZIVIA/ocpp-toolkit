614243cf48aaab67620f7f895cbda20523b09aa4
Edward Poot <edwardmp@gmail.com>
2024-12-03 17:22:22 +0100

    Change here too

--- ocpp-transport/src/main/kotlin/com/izivia/ocpp/transport/OcppVersion.kt
+++ ocpp-transport/src/main/kotlin/com/izivia/ocpp/transport/OcppVersion.kt
@@ -1,5 +1,5 @@
 package com.izivia.ocpp.transport
 
 enum class OcppVersion(val subprotocol: String) {
-    OCPP_1_5("ocpp1.5"), OCPP_1_6("ocpp1.6"), OCPP_2_0("ocpp2.0")
+    OCPP_1_5("ocpp1.5"), OCPP_1_6("ocpp1.6"), OCPP_2_0("ocpp2.0.1")
 }
